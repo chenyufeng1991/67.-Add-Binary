6c04ac7ddb89ed8edcf6f117af77ab7125955982
陈宇峰 <yufengcode@gmail.com>
2016-10-04 18:05:30 +0800

    Initial Commit

---
+++ 67. Add Binary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  67. Add Binary
+//
+//  Created by chenyufeng on 10/4/16.
+//  Copyright © 2016 chenyufeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
