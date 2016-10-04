60c5dd36c449ffc81830fa0c4096177e6c8aecd7
陈宇峰 <yufengcode@gmail.com>
2016-10-04 23:04:09 +0800

    init

--- 67. Add Binary/main.cpp
+++ 67. Add Binary/main.cpp
@@ -7,9 +7,83 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+string addBinary(string a, string b)
+{
+    vector<char> A;
+    vector<char> B;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        A.push_back(a[i]);
+    }
+
+    for (int i = 0; i < b.length(); i++)
+    {
+        B.push_back(b[i]);
+    }
+
+    long lengthR = A.size() >= B.size() ? A.size() + 1 : B.size() + 1;
+    vector<char> result(lengthR);
+    // 初始化
+    for (int i = 0; i < lengthR; i++)
+    {
+        result[i] = '0';
+    }
+
+    // 逆转后再进行加运算
+    reverse(A.begin(), A.end());
+    reverse(B.begin(), B.end());
+    for (int i = 0; i < lengthR - 1; i++)
+    {
+        if (A[i] == '1' && B[i] == '1')
+        {
+            result[i + 1] = '1';
+        }
+        else if (A[i] == '0' && B[i] == '0')
+        {
+
+        }
+        else
+        {
+            if (result[i] == '1')
+            {
+                result[i] = '0';
+                result[i + 1] = '1';
+            }
+            else
+            {
+                result[i] = '1';
+            }
+        }
+    }
+
+    string resultStr;
+    for (int i = 0; i < lengthR; i++)
+    {
+        resultStr += result[i];
+    }
+
+    reverse(resultStr.begin(), resultStr.end());
+
+    return resultStr;
+}
+
+int main(int argc, const char * argv[])
+{
+
+    string ss = addBinary("111", "10");
+    cout << ss;
+
     return 0;
 }
+
+
+
+
+
+
